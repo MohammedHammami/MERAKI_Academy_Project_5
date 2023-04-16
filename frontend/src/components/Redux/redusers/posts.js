f82a113830905bfc8acac410063fe74b2fa9ad59
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-16 16:42:43 +0300

    create posts reducer

---
+++ frontend/src/components/Redux/redusers/posts.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const postSlice = createSlice({
+    name:'post',
+    initialState:{
+        posts:[]
+    },
+    reducers:{
+        
+    }
+
+})
+
+export const {} = postSlice.actions
+export default postSlice.reducers
