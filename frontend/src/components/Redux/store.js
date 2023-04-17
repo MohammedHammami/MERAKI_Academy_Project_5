ec9364dbcb29867c72a648f14badf1e11cb96fd9
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-17 23:47:23 +0300

    add order reducer and react file

--- frontend/src/components/Redux/store.js
+++ frontend/src/components/Redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./redusers/auth";
 import { postSlice } from "./redusers/posts";
 import {craftsSlice } from "./redusers/crafts";
+import {orderSlice } from "./redusers/order";
 export default configureStore ({
     reducer :{
         auth:  authSlice.reducer,
         post:  postSlice.reducer,
-        craft: craftsSlice.reducer
+        craft: craftsSlice.reducer,
+        order: orderSlice.reducer
     }
 })
