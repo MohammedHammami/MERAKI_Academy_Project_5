8e580e1e8d9c6d99dffae06af390983d31ea7ec7
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-18 02:55:09 +0300

    solve error many file reducer

--- frontend/src/components/Redux/reducers/auth.js
+++ frontend/src/components/Redux/reducers/auth.js
@@ -1,26 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
+import jwtDecode from "jwt-decode";
+
 export const authSlice=createSlice({
   name :'auth',
   initialState:{
     token :null,
     userId :null,
     isLoggedIn :false,
-    userInfo:{}
+    userInfo:null
   },
   reducers:{
     setLogin :(state,action)=>{
-        state.token=action.payload
+        state.token=action.payload.token
+        state.userId=action.payload.userId
         state.isLoggedIn=true
         localStorage.setItem(state.token,'token')
-        console.log('done token');
-    },
-    setUserId  :(state,action)=>{
-        state.userId=action.payload
-        
         localStorage.setItem(state.userId,'userId')
-        console.log('done userId');
 
+        console.log('done token and  _userId');
     },
+    
     setLogout :(state)=>{
         state.token =null
         state.userId=null
@@ -28,13 +27,13 @@ export const authSlice=createSlice({
         localStorage.clear()
     },
     setUserInfo :(state,action)=>{
-      state.userInfo = action.payload
+      state.userInfo = jwtDecode(action.payload.token)
+      console.log('state.userInfo:',state.userInfo);
     }
   }
 
 })
 export const {setLogin,
-    setUserId,
-    setLogout}=authSlice.actions
+    setLogout,setUserInfo}=authSlice.actions
     
 export default authSlice.reducer
