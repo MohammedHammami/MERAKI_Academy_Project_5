8d1d599d7e1ab16e838386652c7c7bac9e9363b9
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-17 21:34:13 +0300

    update backend craft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Register from "./components/Register";
 import Login from "./components/Login/Login";
 import CreatePost from "./components/CreatePost";
-import CreateCrafte from "./components/CreateCrafts";
+import CreateCraft from "./components/CreateCrafts";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> </h1>
       </header>
-      <Routes>
-      <Route path="/Register" element={<Register />} />
     <Navbar></Navbar>
       <Routes>
+      <Route path="/Register" element={<Register />} />
         <Route path="/Register" element={<Register />} />
 
         <Route path="/login" element={<Login />} />
