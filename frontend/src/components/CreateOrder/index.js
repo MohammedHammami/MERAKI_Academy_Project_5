433807d39beff33a36bd47992c66e4302cd46879
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-18 06:54:12 +0300

    create order work now with axios right

--- frontend/src/components/CreateOrder/index.js
+++ frontend/src/components/CreateOrder/index.js
@@ -2,30 +2,44 @@ import {useDispatch,useSelector } from "react-redux"
 import React, { useEffect, useState } from 'react'
 
 import axios from "axios"
-import {setCrafts} from "../Redux/reducers/crafts"
-import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
 
 
 const CreateOrder = () =>{
+    const state = useSelector((state) => {
+        return {
+            token:state.auth.token,
+        };
+      });
+    const [schedule_date, setSchedule_date] = useState("")
+    const [order_desc, setOrder_desc] = useState("")
+    const [receiver_user_id,setReceiver_user_id] = useState(4)
+
+    const submitFn = ()=>{
+        axios
+        .post(`http://localhost:5000/orders`,{schedule_date,order_desc,receiver_user_id},{headers: {
+            Authorization: state.token
+            }})
+        .then((result)=>{
+            console.log(result);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
     return(
         <div className="inpust-post">
          <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>schedule_date</Form.Label>
-
-                <Form.Control type="date" placeholder="Enter Title"/>
-
+                <Form.Control type="date" placeholder="Enter Title" onChange={(e)=>{setSchedule_date(e.target.value)}}/>
             </Form.Group>
-
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Description order</Form.Label>
-
-                <Form.Control as="textarea" rows={3} placeholder="Enter description"/>
+                <Form.Control as="textarea" rows={3} placeholder="Enter description" onChange={(e)=>{setOrder_desc(e.target.value)}}/>
             </Form.Group>
          </Form>
-        <button >Submit</button>
-
+        <button onClick={submitFn}>Submit</button>
          </div>
     )
 }
