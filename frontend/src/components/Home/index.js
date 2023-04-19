f643091627f32a32c940280312c62b94b77a6f82
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-19 17:50:50 +0300

    solve logic error in get all posts

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -12,7 +12,7 @@ const Home = () => {
         axios
             .get("http://localhost:5000/posts/")
             .then((res)=>{
-                dispatch(setPost(res.data))
+                dispatch(setPost(res.data.posts))
             })
             .catch((err)=>{
                 console.log(err);
@@ -24,9 +24,9 @@ const Home = () => {
 
     const dispatch = useDispatch();
     const state = useSelector((state) => {
-        console.log(state.post.posts);
+        console.log("sssss",state.post.posts);
         return {
-          posts: state.post.posts.posts,
+          posts: state.post.posts,
         };
       });
 
@@ -51,7 +51,7 @@ const Home = () => {
       <div className="cards">
         {
             state.posts.map((post,i)=>{
-                return <Card className="post" style={{ width: "200", height : "150"}}>
+                return <Card className="post" style={{ width: "200", height : "150"}} key={i}>
                 <Card.Img variant="top" src="https://www.shutterstock.com/image-photo/roofer-carpenter-working-on-roof-260nw-748292161.jpg" />
                 <Card.Body>
                   <Card.Title>{post.title}</Card.Title>
