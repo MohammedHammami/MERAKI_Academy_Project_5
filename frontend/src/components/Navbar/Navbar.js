d7b9756fea0888905a848b8a966637126270d66f
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2023-04-19 23:34:58 +0300

    navbar refactoring

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,21 +1,35 @@
-import React from 'react'
-import Nav from 'react-bootstrap/Nav';
-import { useSelector } from 'react-redux';
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 const Navbar = () => {
-  
+  const state = useSelector((state) => {
+    console.log(state);
+    return {
+      isLoggedIn: state.auth.isLoggedIn,
+    };
+  });
+  console.log(state.isLoggedIn);
+
   return (
-    <div>
+    <>
+      {state.isLoggedIn ? (
+        <>
+          <div className="navbar">
+            <Link to="/Register"> Register </Link>
+            <Link to="/login"> login </Link>
+            <Link to="/CreateCraft"> Craft </Link>
+            <Link to="/Comment"> Comment </Link>
+            <Link ></Link>
+          </div>
+        </>
+      ) : (
+        <>
+        
+        </>
+      )}
+    </>
+  );
+};
 
-    <div className='navbar' >
-      
-      <Link to="/Register"> Register </Link>
-      <Link to="/login"> login </Link>
-      <Link to="/CreateCraft"> Craft </Link>
-      <Link to="/Comment"> Comment </Link>
-    </div>
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
