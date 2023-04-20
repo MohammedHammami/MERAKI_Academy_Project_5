98e0ab03a055126375fdc1bdd532030e799b980a
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-20 07:45:38 +0300

    first build for graph reviews

--- frontend/src/components/DashboardProvider/index.js
+++ frontend/src/components/DashboardProvider/index.js
@@ -1,9 +1,38 @@
-import "./style.css"
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
 
 const DashboardProvider = () =>{
+    const ChartComponent = (props) => {
+        const chartRef = useRef(null);
+      
+        useEffect(() => {
+          const chartCanvas = chartRef.current.getContext("2d");
+          const myChart = new Chart(chartCanvas, {
+            type: "bar",//bar,
+            data: {
+                labels: [],
+                datasets: [
+                {
+                    label: "My First Dataset",
+                    data: [],
+                    backgroundColor: "rgb(255, 99, 132)",
+                    borderColor: "rgba(255, 99, 132, 0.2)"
+                }
+                ]
+            },
+            options: {scales: {y:{beginAtZero: true}}}
+          });
+      
+          return () => {
+            myChart.destroy();
+          };
+        }, [props]);
+      
+        return <canvas ref={chartRef} />;
+      };
     return (
         <div className="container">
-            <p>i am in dashboar</p>
+            <ChartComponent/>
         </div>
     )
 }
