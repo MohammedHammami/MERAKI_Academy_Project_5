c037f861a4ab194de4dac13996dd9a7d13c5c215
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-20 11:50:54 +0300

    first build for dashboard provider (menu and card for orders)

--- frontend/src/components/DashboardProvider/index.js
+++ frontend/src/components/DashboardProvider/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import Chart from "chart.js/auto";
-
+import "./style.css"
 const DashboardProvider = () =>{
     const ChartComponent = (props) => {
         const chartRef = useRef(null);
@@ -31,8 +31,34 @@ const DashboardProvider = () =>{
         return <canvas ref={chartRef} />;
       };
     return (
-        <div className="container">
-            <ChartComponent/>
+        <div className="container_dashboard_provider">
+          <div className="menu">
+            <p>menu</p>
+            <p>profile</p>
+            <p>posts</p>
+            <p>order</p>
+            <p>notification</p>
+            <p>createPost</p>
+          </div>
+          <div className="body_container">
+            <div className="order_info__cotainer_div">
+              <div className="card_order_info">
+                <h3>no: of order Completed</h3>
+                <p>sssssssss</p>
+                <p>sssssssss</p>
+              </div>
+              <div className="card_order_info">
+                <h3>no: of order Pending</h3>
+                <p>sssssssss</p>
+              </div>
+              <div className="card_order_info">
+                <h3>no: of order Canceled</h3>
+                <p>sssssssss</p>
+              </div>
+
+            </div>
+          </div>
+            {/* <ChartComponent/> */} 
         </div>
     )
 }
