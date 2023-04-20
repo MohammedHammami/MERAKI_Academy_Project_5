5d146ac2a2bfd46f215c9bd894969387c77d9dab
ahmade-shehadeh <ahmade.shehadeh@gmail.com>
2023-04-21 02:43:21 +0300

    creat comment V-04

--- frontend/src/components/Comments/Comments.js
+++ frontend/src/components/Comments/Comments.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import './Comment.css' 
 import axios from "axios";
 import { useSelector } from "react-redux";
 import {
@@ -32,7 +33,7 @@ const Comments = () => {
         },
       })
       .then((result) => {
-        // console.log(result.data.result);
+        console.log(result.data.result);
         setComments(result.data.result);
       })
       .catch((err) => {
@@ -49,7 +50,10 @@ const Comments = () => {
       })
       .then((result) => {
         console.log(result.data.result);
-        setComments(result.data.result);
+const newresult= result.data.result[0]
+        const newComment =[...comments,newresult]
+        console.log(newComment);
+        setComments(newComment)
       })
       .catch((err) => {
         console.log(err);
@@ -66,11 +70,7 @@ const Comments = () => {
     <div>
       <h1>COMMENT</h1>
       <div>
-      <input type="text" placeholder="youer comment" onChange={(e)=>{
-        setDescription(e.target.value)
-      }}></input>
-      <button onClick={()=>{CreateComment(3)}}>send </button>
-
+     
       </div>
       <MDBContainer className="mt-5" style={{ maxWidth: "1000px" }}>
         <MDBRow className="justify-content-center">
@@ -85,11 +85,10 @@ const Comments = () => {
           setDescription(e.target.value)
         }} />
                 <button onClick={()=>{CreateComment(3)}}>send </button>
-              <MDBCard className="mb-4">
-                {console.log(comments)}d
                 {comments && comments.map(comment =>{
                   return (
-                  <div key={comment.id}>
+                    <MDBCard className="mb-4">
+                  <div className="comment" key={comment.id}>
                   <MDBCardBody>
                     {console.log(comment.description)}
                   <p>{comment.description}</p>
@@ -104,23 +103,32 @@ const Comments = () => {
                       />
                       <p className="small mb-0 ms-2">{state.userInfo.first_name}</p>
                     </div>
+                    
                     <div className="d-flex flex-row align-items-center">
-                    
-                      
-                    
+                      {/* <p className="small text-muted mb-0">created_on</p> */}
+                      {/* <MDBIcon
+                        far
+                        icon="thumbs-up mx-2 fa-xs text-black"
+                        style={{ marginTop: "-0.16rem" }}
+                      /> */}
+                      <p className="small text-muted mb-4 created_on">{comment.created_on && comment.created_on.split("").splice(0,10)}</p>
                     </div>
                   </div>
+                 
+      
 
-      </MDBCardBody></div>
-      )
-                })}
-                
+      </MDBCardBody>
+      </div>
     </MDBCard>
+      )
+    })}
+                
               </MDBCardBody>
       </MDBCard>
           </MDBCol>
       </MDBRow>
      </MDBContainer>
+     {console.log('2023-04-20T17:30:02.716Z'.split("").splice(0,8))}
     </div>
   );
 };
