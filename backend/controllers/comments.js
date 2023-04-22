eada1332f087310fbea00bc710eef08782769900
ahmade-shehadeh <ahmade.shehadeh@gmail.com>
2023-04-22 18:05:51 +0300

    backEnd Delete comment V02

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -27,7 +27,6 @@ const receiver_user_id =req.params.id
 
 const getCommentsByUser = (req, res) =>{
     const userId = req.params.id
-    console.log(userId);
     const queryString = `SELECT * FROM comments WHERE receiver_user_id = ${userId};`
     
     pool
@@ -47,26 +46,44 @@ const getCommentsByUser = (req, res) =>{
             })
         })
 }
-
 const deleteCommentById = (req, res) =>{
     const id =  req.params.id 
     const queryString = `DELETE FROM comments where id = ${id};`
-
     pool
-        .query(queryString)
-        .then((result)=>{   
-            res.status(204).json({
-                success: true,
-                massage: `comment with id: ${id} deleted successfully`
+        .query( ` SELECT * FROM "comments" WHERE id=${id}`)
+        .then((result1)=>{ 
+         if (result1.rows.length==0) {
+            console.log(60);
+            res.status(404).json({
+                success: false +'5',
+                message: `comment with id: ${id} Not Found`,
+                
+            }) 
+         }else {
+            pool
+            .query(queryString)
+            .then((result)=>{   
+                res.status(202).json({
+                    success: true,
+                    massage: `comment with id: ${id} deleted successfully`,
+                    result:result1.rows[0]
+                })
             })
+            .catch((err)=>{
+                res.status(500).json({
+                    success: false+ '6',
+                    message: "Server error",
+                    error : err
+                })              
+            })
+         }
         })
         .catch((err)=>{
             res.status(500).json({
                 success: false,
                 message: "Server error",
                 error : err
-            })              
-        })
+            })    })       
 }
 
 module.exports ={
