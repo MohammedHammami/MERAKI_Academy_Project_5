f8659ddeadc0c9f4ebfac3bfe9b2cc21e8dcc21a
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2023-04-24 22:58:18 +0300

    update post modal

--- frontend/src/components/UsersPosts/index.js
+++ frontend/src/components/UsersPosts/index.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setPost, deletePost, updatePost } from "../Redux/reducers/posts";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import Form from 'react-bootstrap/Form';
 const UserPost = () => {
+  const [show, setShow] = useState(false);
+
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const state = useSelector((state) => {
     return {
       token: state.auth.token,
@@ -50,7 +58,7 @@ const UserPost = () => {
 
   const updateSelectedPost = (id) => {
     axios
-      .put(`http://localhost:5000/posts/${id}`, {
+      .put(`http://localhost:5000/posts/${id}`,{ title, description } ,{
         headers: {
           Authorization: `Bearer ${state.token}`,
         },
@@ -73,34 +81,69 @@ const UserPost = () => {
     <div className="cards">
       {state.posts.map((post, i) => {
         return (
-          <Card
-            className="post"
-            style={{ width: "200", height: "150" }}
-            key={i}
-          >
-            <Card.Img
-              variant="top"
-              src="https://www.shutterstock.com/image-photo/roofer-carpenter-working-on-roof-260nw-748292161.jpg"
-            />
-            <Card.Body>
-              <Card.Title>{post.title}</Card.Title>
-              <Button
-                onClick={(e) => {
-                  deleteSelectedPost(post.id);
-                }}
-              >
-                delete post
-              </Button>
-              <br />
-              <Button
-                onClick={(e) => {
-                    updateSelectedPost(post.id);
-                }}
-              >
-                update post
-              </Button>
-            </Card.Body>
-          </Card>
+          <>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form.Control
+                className="mb-2"
+                  type="text"
+                  id="text"
+                  placeholder="new title"
+                  onChange={(e)=>{setTitle(e.target.value)}}
+                />
+                <Form.Control
+                  type="text"
+                  id="text"
+                  placeholder="new description"
+                  onChange={(e)=>{setDescription(e.target.value)}}
+                />
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={
+                    (e)=>{
+                        handleClose()
+                        updateSelectedPost(id)
+                    }
+                }>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+            <Card
+              className="post"
+              style={{ width: "200", height: "150" }}
+              key={i}
+            >
+              <Card.Img
+                variant="top"
+                src="https://www.shutterstock.com/image-photo/roofer-carpenter-working-on-roof-260nw-748292161.jpg"
+              />
+              <Card.Body>
+                <Card.Title>{post.title}</Card.Title>
+                <Button
+                  onClick={(e) => {
+                    deleteSelectedPost(post.id);
+                  }}
+                >
+                  delete post
+                </Button>
+                <br />
+                <Button
+                  onClick={(e) => {
+                    handleShow();
+                  }}
+                >
+                  update post
+                </Button>
+              </Card.Body>
+            </Card>
+          </>
         );
       })}
     </div>
