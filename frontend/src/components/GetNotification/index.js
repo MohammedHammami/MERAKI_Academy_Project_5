a7db7ee783ad9df4a31c1361e34b7beda64793dd
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-25 19:43:00 +0300

    update on frontEnd notification

--- frontend/src/components/GetNotification/index.js
+++ frontend/src/components/GetNotification/index.js
@@ -28,27 +28,29 @@ const GetAllNotification = ()=>{
     useEffect(()=>{
         getNotifications()
     },[])
-    const createNotivication = (order_id)=>{
+    const createNotivication = (order_id,description,status)=>{
         axios
         .get(`http://localhost:5000/orders/order_id/${order_id}`)
         .then((result)=>{
             console.log(result);
             axios
-            .post(`http://localhost:5000/notifications/${order_id}`,{description:`description:"your order accepted"`,status:"accept_order",receiver_user_id:result.data.order[0].requester_user_id})
+            .post(`http://localhost:5000/notifications/${order_id}`,{description:description,status:status,receiver_user_id:result.data.order[0].requester_user_id})
             .then((result)=>{console.log(result);})
             .catch((err)=>{console.log(err);})
 
         })
         .catch((err)=>{console.log(err);}) 
         }
-    const acceptFn = (id) => {
+    const updateNotificationFn = (id,status) => {
         axios
-        .put(`http://localhost:5000/notifications/${id}`,{status:"accept_order"})
+        .put(`http://localhost:5000/notifications/${id}`,{status:status})
         .then((result)=>{console.log(result);})
         .catch((err)=>{console.log(err);})
     }
     return (    
     <div className="all-notification-div">
+        <div className="notification">
+            <p>notification create_order</p>
         {
             notifications.map((noti,i)=>{
                 if(noti.status==="create_order"){
@@ -56,13 +58,53 @@ const GetAllNotification = ()=>{
                 <Card key={i}>
                     <Card.Body>
                         <Card.Title>{noti.description}</Card.Title>
-                        <Button onClick={()=>{createNotivication(noti.order_id);acceptFn(noti.id)}}>Accept</Button>
+                        <Button onClick={()=>{
+                            createNotivication(noti.order_id,"your order accepted from provider","accept_order");
+                            updateNotificationFn(noti.id,"accepted_order")}}>Accept</Button>
+                        <Button onClick={()=>{
+                            createNotivication(noti.order_id,"your order canceld from provider","canceld_order");
+                            updateNotificationFn(noti.id,"order_canceld")}}>Cancel</Button>
+                    </Card.Body>
+                </Card>
+                )}
+            })
+        }
+        </div>
+        <div className="notification">
+            <p>notification accept_order</p>
+        {
+            notifications.map((noti,i)=>{
+                if(noti.status==="accept_order"){
+                return(
+                <Card key={i}>
+                    <Card.Body>
+                        <Card.Title>{noti.description}</Card.Title>
+                        <p>the provider accepted</p>
+                        <p>please rate the provider</p>
+                        <Button>rate</Button>
+                    </Card.Body>
+                </Card>
+                )}
+            })
+        }
+        </div>
+        <div className="notification">
+            <p>notification canceld_order</p>
+        {
+            notifications.map((noti,i)=>{
+                if(noti.status==="canceld_order"){
+                return(
+                <Card key={i}>
+                    <Card.Body>
+                        <Card.Title>{noti.description}</Card.Title>
+                        <p>the provider canceld order</p>
                         <Button>Cancel</Button>
                     </Card.Body>
                 </Card>
                 )}
             })
         }
+        </div>
     </div>
     )   
 }
