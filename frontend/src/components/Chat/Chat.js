4c7040b6c8666de0b563111039bd3e17ea7b420d
ahmade-shehadeh <ahmade.shehadeh@gmail.com>
2023-04-28 10:21:59 +0300

    clean cood Chat

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,37 +1,12 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { Configuration, OpenAIApi } from "openai";
-
 import "./Chat.css";
 import axios from "axios";
 const Chat = () => {
-const configuration = new Configuration({
-    organization: "org-FZee788mL02ULw9DVXe7MJ7F",
-    apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
-
   const [content, setContent] = useState("");
   const [output, setOutput] = useState("");
-  const handleMessageSubmit = async () => {
-    const apiKey = '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';
-    const endpoint = 'https://api.chatgpt.com/v1/chat';
-
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-        message: content
-      })
-    });
-}
-  const ChatNow = async() => { 
-    
-    const apiKey = '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';
-
+  const ChatNow = async () => {
+    const apiKey = "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";
     const requset = {
       model: "gpt-3.5-turbo",
       messages: [
@@ -44,15 +19,14 @@ const openai = new OpenAIApi(configuration);
       max_tokens: 200,
     };
     axios
-      .post(`https://api.openai.com/v1/chat/completions`,requset, {
-        headers:{
-            'Content-Type': 'application/json',
-            authorization: `Bearer ${apiKey} `
-        }
+      .post(`https://api.openai.com/v1/chat/completions`, requset, {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${apiKey} `,
+        },
       })
       .then((result) => {
-        console.log(result.data.choices[0].message.content);
-        setOutput(result.data.choices[0].message.content)
+        setOutput(result.data.choices[0].message.content);
       })
       .catch((err) => {
         console.log(err);
@@ -66,27 +40,28 @@ const openai = new OpenAIApi(configuration);
           className="word-input"
           type="search"
           placeholder="Inquire about services & maintenance"
-          onChange={(e)=>{
-const value=e.target.value
-setContent(value)
+          onChange={(e) => {
+            const value = e.target.value;
+            setContent(value);
           }}
         ></input>
         <Button size="sm" className="submit-btn" onClick={ChatNow}>
           {" "}
           Submit
         </Button>
-        <Button size="sm" className="submit-btn" onClick={()=>{
-            setOutput('')
-        }}>
+        <Button
+          size="sm"
+          className="submit-btn"
+          onClick={() => {
+            setOutput("");
+          }}
+        >
           {" "}
           Clear
         </Button>
       </div>
-      
-      <div className="reply-content " style={{  overflow: 'auto' }}>
-      <div style={{ maxHeight: '100%', overflow: 'scroll' }}>
-        {output} 
-       </div>
+      <div className="reply-content " style={{ overflow: "auto" }}>
+        <div style={{ maxHeight: "100%", overflow: "scroll" }}>{output}</div>
       </div>
     </div>
   );
