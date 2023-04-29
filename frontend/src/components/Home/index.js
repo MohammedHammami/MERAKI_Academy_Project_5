5753a6d26850643ae6e7259f3a548e6baab5ae82
MohammedHammami <mohammed.hammammi@gmail.com>
2023-04-29 09:24:14 +0300

    update side bar

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -45,19 +45,19 @@ const Home = () => {
   return (
     <>
       <header style={{ paddingLeft: 0 }}>
-      <section id="hero" class="hero d-flex align-items-center section-bg">
-    <div class="container">
-      <div class="row justify-content-between gy-5">
-        <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-center text-lg-start">
+      <section id="hero" className="hero d-flex align-items-center section-bg">
+    <div className="container">
+      <div className="row justify-content-between gy-5">
+        <div className="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-center text-lg-start">
           <h2 data-aos="fade-up">Maintenance Is<br/>Easier Than Before</h2>
           <p data-aos="fade-up" data-aos-delay="100">A well trained aramy of Craftsmen ready for your services</p>
-          <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-            <a href="#book-a-table" class="btn-book-a-table">Book an Appointment</a>
-            <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+          <div className="d-flex" data-aos="fade-up" data-aos-delay="200">
+            <a href="#book-a-table" className="btn-book-a-table">Book an Appointment</a>
+            <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>Watch Video</span></a>
           </div>
         </div>
-        <div class="col-lg-5 order-1 order-lg-2 text-center text-lg-start">
-          <img src="https://img.freepik.com/free-vector/illustration-characters-fixing-cogwheel_53876-40796.jpg?w=2000" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="300"/>
+        <div className="col-lg-5 order-1 order-lg-2 text-center text-lg-start">
+          <img src="https://img.freepik.com/free-vector/illustration-characters-fixing-cogwheel_53876-40796.jpg?w=2000" className="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="300"/>
         </div>
       </div>
     </div>
