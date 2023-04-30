b554a96f2e658b1a38d4b1ddce8cbfec959e5d8f
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2023-04-30 10:20:39 +0300

    styling support page

--- frontend/src/components/Support/index.js
+++ frontend/src/components/Support/index.js
@@ -2,44 +2,44 @@ import "./style.css"
 
 const Support = () => {
     return (   
-        <section id="contact" class="contact" style={{paddingTop:"0"}}>
+        <section id="contact" className="contact" style={{paddingTop:"0"}}>
 
-        <div class="container" data-aos="fade-up" style={{paddingTop:"0"}}>
+        <div className="container" data-aos="fade-up" style={{paddingTop:"0"}}>
   
-          <header class="section-header">
+          <header className="section-header">
             <h2>Contact</h2>
             <p>Contact Us</p>
           </header>
   
-          <div class="row gy-4">
+          <div className="row gy-4">
   
-            <div class="col-lg-6">
+            <div className="col-lg-6">
   
-              <div class="row gy-4">
-                <div class="col-md-6">
-                  <div class="info-box">
-                    <i class="bi bi-geo-alt"></i>
+              <div className="row gy-4">
+                <div className="col-md-6">
+                  <div className="info-box">
+                    <i className="bi bi-geo-alt"></i>
                     <h3>Address</h3>
                     <p>A108 Ali Hussien Street,<br/>Amman, Jubieha 11943</p>
                   </div>
                 </div>
-                <div class="col-md-6">
-                  <div class="info-box">
-                    <i class="bi bi-telephone"></i>
+                <div className="col-md-6">
+                  <div className="info-box">
+                    <i className="bi bi-telephone"></i>
                     <h3>Call Us</h3>
                     <p>+962 77777777<br/>+962 88888888</p>
                   </div>
                 </div>
-                <div class="col-md-6">
-                  <div class="info-box">
-                    <i class="bi bi-envelope"></i>
+                <div className="col-md-6">
+                  <div className="info-box">
+                    <i className="bi bi-envelope"></i>
                     <h3>Email Us</h3>
                     <p>creativeminds@taslee7.com<br/>contact@taslee7.com</p>
                   </div>
                 </div>
-                <div class="col-md-6">
-                  <div class="info-box">
-                    <i class="bi bi-clock"></i>
+                <div className="col-md-6">
+                  <div className="info-box">
+                    <i className="bi bi-clock"></i>
                     <h3>Open Hours</h3>
                     <p>Sunday - Thursday<br/>9:00AM - 05:00PM</p>
                   </div>
@@ -48,30 +48,30 @@ const Support = () => {
   
             </div>
   
-            <div class="col-lg-6">
-              <form action="forms/contact.php" method="post" class="php-email-form">
-                <div class="row gy-4">
+            <div className="col-lg-6">
+              <form action="forms/contact.php" method="post" className="php-email-form">
+                <div className="row gy-4">
   
-                  <div class="col-md-6">
-                    <input type="text" name="name" class="form-control" placeholder="Your Name" required/>
+                  <div className="col-md-6">
+                    <input type="text" name="name" className="form-control" placeholder="Your Name" required/>
                   </div>
   
-                  <div class="col-md-6 ">
-                    <input type="email" class="form-control" name="email" placeholder="Your Email" required/>
+                  <div className="col-md-6 ">
+                    <input type="email" className="form-control" name="email" placeholder="Your Email" required/>
                   </div>
   
-                  <div class="col-md-12">
-                    <input type="text" class="form-control" name="subject" placeholder="Subject" required/>
+                  <div className="col-md-12">
+                    <input type="text" className="form-control" name="subject" placeholder="Subject" required/>
                   </div>
   
-                  <div class="col-md-12">
-                    <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+                  <div className="col-md-12">
+                    <textarea className="form-control" name="message" rows="6" placeholder="Message" required></textarea>
                   </div>
   
-                  <div class="col-md-12 text-center">
-                    <div class="loading">Loading</div>
-                    <div class="error-message"></div>
-                    <div class="sent-message">Your message has been sent. Thank you!</div>
+                  <div className="col-md-12 text-center">
+                    <div className="loading">Loading</div>
+                    <div className="error-message"></div>
+                    <div className="sent-message">Your message has been sent. Thank you!</div>
   
                     <button type="submit">Send Message</button>
                   </div>
