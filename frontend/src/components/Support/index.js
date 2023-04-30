89e853d481027ea1f511bebdb68f96f3c9c67609
AnasMBajawi <anas.mohammad.bajawi@gmail.com>
2023-04-30 10:20:07 +0300

    styling support page

--- frontend/src/components/Support/index.js
+++ frontend/src/components/Support/index.js
@@ -20,28 +20,28 @@ const Support = () => {
                   <div class="info-box">
                     <i class="bi bi-geo-alt"></i>
                     <h3>Address</h3>
-                    <p>A108 Adam Street,<br/>New York, NY 535022</p>
+                    <p>A108 Ali Hussien Street,<br/>Amman, Jubieha 11943</p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="info-box">
                     <i class="bi bi-telephone"></i>
                     <h3>Call Us</h3>
-                    <p>+1 5589 55488 55<br/>+1 6678 254445 41</p>
+                    <p>+962 77777777<br/>+962 88888888</p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="info-box">
                     <i class="bi bi-envelope"></i>
                     <h3>Email Us</h3>
-                    <p>info@example.com<br/>contact@example.com</p>
+                    <p>creativeminds@taslee7.com<br/>contact@taslee7.com</p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="info-box">
                     <i class="bi bi-clock"></i>
                     <h3>Open Hours</h3>
-                    <p>Monday - Friday<br/>9:00AM - 05:00PM</p>
+                    <p>Sunday - Thursday<br/>9:00AM - 05:00PM</p>
                   </div>
                 </div>
               </div>
